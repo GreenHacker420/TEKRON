063ef2c9dbd3d7ce4a3435bc2c7378e1d1fbc474
SaadArqam <saadarqamrnc@gmail.com>
2025-02-15 20:26:33 +0530

    js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function updateCountdown() {
         const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-        document.getElementById("countdown").innerHTML = `${days}d:${hours}h:${minutes}m:${seconds}s`;
+        document.getElementById("countdown").innerHTML = `${days}:${hours}:${minutes}:${seconds}`;
     } else {
         document.getElementById("countdown").innerHTML = "Event Started!";
     }
