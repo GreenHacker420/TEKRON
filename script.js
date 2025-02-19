b8733c254570521b52d5339df5889ba7d44ca366
DSPROCODER <sainidevansh1308@gmail.com>
2025-02-19 12:35:21 +0530

    Updated media for phone

--- script.js
+++ script.js
@@ -20,3 +20,10 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
+
+var elem = document.getElementsByClassName("text-content");
+elem.remove();
+
+if (window.innerWidth < 768) {
+    document.getElementsByClassName("text-content").remove();
+}
